5fa8b2547beebcd2d2143bdd05fb72eb16469062
Rob Cruz <robert.cruz@gmail.com>
2018-12-11 13:09:14 -0800

    Mail BVTs

--- features/step_definitions/history/history_details_steps.rb
+++ features/step_definitions/history/history_details_steps.rb
@@ -31,7 +31,7 @@ Then /^expect address shipped to contains (\d+) lines$/ do |lines|
 end
 
 Then /^set search prints tracking number on history filter panel to (?:newly added|(.*))$/ do |str|
-  search =  SdcHistory.filter_panel.search
+  search = SdcHistory.filter_panel.search
   #SdcPage.browser.driver.action.send_keys(:esc)
   search.search_prints.safe_wait_until_present(timeout: 10)
   str ||= TestData.hash[:tracking_number]
@@ -39,14 +39,14 @@ Then /^set search prints tracking number on history filter panel to (?:newly add
 end
 
 Then /^click search icon on history filter panel$/ do
-  search =  SdcHistory.filter_panel.search
+  search = SdcHistory.filter_panel.search
   search.search_icon.click
 end
 
 Then /^set search prints to (?:newly added|(.*)) tracking number on history filter panel$/ do |str|
-  search =  SdcHistory.filter_panel.search
+  search = SdcHistory.filter_panel.search
   search.search_prints.safe_wait_until_present(timeout: 10)
-  str||= TestData.hash[:tracking_number]
+  str ||= TestData.hash[:tracking_number]
   search.search_prints.set(str)
 end
 Then /^expect history filter panel search results tab is present$/ do
@@ -55,33 +55,33 @@ end
 
 Then /^expect recipient column include (.*)$/ do |str|
   history_search_results = SdcHistory.filter_panel.search_results
-search_count = history_search_results.search_results_count.text_value.to_i
-p search_count
-j =1
-while j<=10
-  SdcLogger.info "value of j is #{j}"
-  recipient_name = Array.new
-  i = 1
-  SdcLogger.info "value of i is #{i}"
+  search_count = history_search_results.search_results_count.text_value.to_i
+  p search_count
+  j = 1
+  while j <= 10
+    SdcLogger.debug "value of j is #{j}"
+    recipient_name = Array.new
+    i = 1
+    SdcLogger.debug "value of i is #{i}"
 
-  while i <= 10
-    p i
-    recipient_name[i] = SdcHistory.grid.grid_column(:recipient).text_at_row(i)
-    tmp = recipient_name[i]
-    SdcLogger.info "value of recipient_name[i] is: #{recipient_name[i]}"
-    SdcLogger.info "value of tmp is: #{tmp}}"
-    p 'next'
-    p str
-    p expect(tmp).to include(str)
-    expect(recipient_name[i].include? str).to be(true)
+    while i <= 10
+      p i
+      recipient_name[i] = SdcHistory.grid.grid_column(:recipient).text_at_row(i)
+      tmp = recipient_name[i]
+      SdcLogger.info "value of recipient_name[i] is: #{recipient_name[i]}"
+      SdcLogger.info "value of tmp is: #{tmp}}"
+      p 'next'
+      p str
+      p expect(tmp).to include(str)
+      expect(recipient_name[i].include? str).to be(true)
 
-    tracking_id = SdcHistory.grid.grid_column(:tracking_number).text_at_row(i)
-    SdcLogger.info "value of  tracking_id[i] is: #{ tracking_id}"
+      tracking_id = SdcHistory.grid.grid_column(:tracking_number).text_at_row(i)
+      SdcLogger.info "value of  tracking_id[i] is: #{ tracking_id}"
+    end
+    j += 1
   end
-  j = j+1
 end
-end 
-  #old code
+#old code
 #   history_search =  SdcHistory.filter_panel.search_results
 #   search_count = history_search.search_results_count.text_value.to_i
 #   p search_count
@@ -107,30 +107,45 @@ end
 
 Then /^expect history pagination works$/ do
   history_gird_pagination = SdcHistory.pagination.history_pagination
+  expect(history_gird_pagination.page_arrow_disabled('next')).to be (false)
+  max_pages_text = history_gird_pagination.max_pages.text_value.split("of ")
+  max_pages = max_pages_text[1]
+  SdcLogger.debug "max_pages #{max_pages}"
 
-  # SdcLogger.info "#{'next'} Page disabled #{pagination.page_arrow_disabled('next')}"
-  if expect(history_gird_pagination.page_arrow_disabled('next')).to be (false)
-    p "inside pagination if"
-    max_pages_text = history_gird_pagination.max_pages.text_value
-    max_pages_text = max_pages_text.split("of ")
-    max_pages = max_pages_text[1]
-    SdcLogger.info "max_pages #{max_pages}"
-
-    i=1
-    while i < max_pages.to_i
-      next_button = SdcHistory.pagination.history_pagination
-      p next_button.page_next.enabled?
-      if expect(next_button.page_next.enabled?).to be(true)
-        step 'click on the pagination next button of history page'
-      else
-        SdcLogger.info "Next arrow is disabled"
-      end
-      i= i+1
+  i = 1
+  while i < max_pages.to_i
+    next_button = SdcHistory.pagination.history_pagination
+    p next_button.page_next.enabled?
+    if expect(next_button.page_next.enabled?).to be(true)
+      step 'click on the pagination next button of history page'
+    else
+      SdcLogger.debug "Next arrow is disabled"
     end
-
+    i += 1
   end
+
 end
 
+# if expect(history_gird_pagination.page_arrow_disabled('next')).to be (false)
+#   max_pages_text = history_gird_pagination.max_pages.text_value
+#   max_pages_text = max_pages_text.split("of ")
+#   max_pages = max_pages_text[1]
+#   SdcLogger.info "max_pages #{max_pages}"
+#
+#   i = 1
+#   while i < max_pages.to_i
+#     next_button = SdcHistory.pagination.history_pagination
+#     p next_button.page_next.enabled?
+#     if expect(next_button.page_next.enabled?).to be(true)
+#       step 'click on the pagination next button of history page'
+#     else
+#       SdcLogger.info "Next arrow is disabled"
+#     end
+#     i += 1
+#   end
+#
+# end
+#
 Then /^click on the pagination next button of history page$/ do
   next_button = SdcHistory.pagination.history_pagination
   next_button.page_next.click
